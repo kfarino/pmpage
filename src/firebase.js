638362471aaa8cffaf8c404ee2d9ff5a36a2860c
kfarino <farinooh@gmail.com>
2025-01-17 14:16:04 -0800

    homepage made

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCey9hd63LmIRAya9HNj72zN_8Z1AWHNOk",
-  authDomain: "pmpage-a8fb8.firebaseapp.com",
-  projectId: "pmpage-a8fb8",
-  storageBucket: "pmpage-a8fb8.firebasestorage.app",
-  messagingSenderId: "590147961688",
-  appId: "1:590147961688:web:f153d6c22fa5e2a3cddb2d",
-  measurementId: "G-P4XYBQJZ1S"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
