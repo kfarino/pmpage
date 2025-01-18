7d38e047d05597a6117d9ad42aecb9333fe8311a
kfarino <farinooh@gmail.com>
2025-01-18 08:35:07 -0800

    updated style

--- src/Views/Header.js
+++ src/Views/Header.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import './Header.css';
-import Kevin from '../assets/images/Kevin.jpg';
+import Kevin from '../assets/images/Kevin.png';
 import Biglogo from '../assets/images/logo512.png';
 
 const Header = () => {
   return (
     <div className="header-container">
-      <nav className="nav">
-        <img src={Biglogo || "/placeholder.svg"} alt="KF Logo" className="logo" />
-      </nav>
       <header className="header">
         <div className="header-content">
           <div className="header-text">
-            <h1 className="title">Transform Your <span className="highlight">Business Growth</span></h1>
+            <h1 className="title">
+              I<span className="highlight">'</span>m<br />
+              KEVIN FARINO<span className="highlight">.</span>
+            </h1>
             <p className="value-prop">
-              Your personal AI-powered Product and Growth Expert
+              <span className="dash">MIT Educated, Silicon Valley Tested.</span> 
             </p>
+            <a 
+              href="https://calendar.app.google/ph26brtsXBXvZwLZ7" 
+              className="cta-button"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Book a Call
+            </a>
             <p className="description">
-              Leverage cutting-edge AI technology and proven growth strategies to scale your business effectively.
+              PRODUCT LAUNCH AND GROWTH EXPERTISE FOR STARTUPS AND SCALEUPS. 
             </p>
-            <button className="cta-button">
-              Schedule a Strategy Session
-            </button>
           </div>
           <div className="header-image">
             <img src={Kevin || "/placeholder.svg"} alt="Kevin Farino" className="profile-image" />
